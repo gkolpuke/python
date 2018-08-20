95a2f0a3dc40843cdc81d89f447d41cf0b37f95d
Gajanan kolpuke <gajanankolpuke@gmail.com>
2018-08-20 23:52:03 +0530

    First program

---
+++ hello_universe.py
@@ -0,0 +1,40 @@
+print("Hello Universe")
+""" This is multiple comment
+11
+22
+33
+"""
+
+x = 'hello'
+type(x)
+
+print(type(x))
+
+print(x[0])
+#print(x[10])
+
+print(x[-1])
+print(x[-4])
+print(x[1:3])
+print(x[::2])
+print(x[3:1:-1])
+print(x[3::-1])
+
+str = 'Gajanan Kolpuke'
+print(str[:9])
+print(str[8:9])
+
+print(str[1:3:-1])
+print(str[:9:8])
+print(str[:9:-2])
+print(str[1:9:-2])
+print(str[1:3:10000])
+print(str[100:])
+print(str[::-1])
+
+y = 'www.sc.com'
+print(y[4:-4:])
+y = input("Enter Name")
+
+print(y)
+z = eval(input("Enter Name"))
