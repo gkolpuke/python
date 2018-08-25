9fc7b5dadc8c6ea2668358c96a164e5d759cdd21
Gajanan kolpuke <gajanankolpuke@gmail.com>
2018-08-25 20:21:23 +0530

    second program

--- assignments.py
+++ assignments.py
@@ -5,4 +5,12 @@ number = eval(input('Enter number: '))
 power = eval(input('Enter power of: '))
 
 result = number ** power
-print(power,'th of Power ', number, ' is = ', result )
+print(power,'th of Power', number,' is = ', result )
+
+''' 2nd program
+W.A.P. to accept string from  user and print it in reverse order.
+'''
+print('\n\n --- Reverse String --- ')
+strString = input('Enter a string: ')
+revString = strString[::-1]
+print('Reverse string is: ' + revString)
