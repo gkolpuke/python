0ce4b39372ffee725bd0b23c28898278670aa645
Gajanan kolpuke <gajanankolpuke@gmail.com>
2018-08-25 20:00:14 +0530

    Assignments file

---
+++ assignments.py
@@ -0,0 +1,8 @@
+#Assignments
+# Date : 25Aug2018
+'''W.A.P to accept two numbers from user and display their power'''
+number = eval(input('Enter number: '))
+power = eval(input('Enter power of: '))
+
+result = number ** power
+print(power,'th of Power ', number, ' is = ', result )
